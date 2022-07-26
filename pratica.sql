5bc188986e2d2eb8bde235842424c1209e586129
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-07-26 20:39:21 -0300

    update

--- pratica.sql
+++ pratica.sql
@@ -1,14 +1,15 @@
 -- QUESTÃO 1 ----------------------------
 
-SELECT users.id , users.name , cities.name AS city  FROM users JOIN cities ON users."cityId" = cities.id WHERE cities.name='Rio de Janeiro' LIMIT 5;
+SELECT users.id , users.name , cities.name AS city  FROM users JOIN cities ON users."cityId" = cities.id WHERE cities.name='Rio de Janeiro';
 
 -- QUESTÃO 2 ----------------------------
 
-SELECT testimonials.id , writer.name AS writer, recipient.name AS recipient ,testimonials.message FROM testimonials JOIN users writer ON testimonials."writerId" = writer.id JOIN users recipient ON testimonials."recipientId" = recipient.id LIMIT 5;
+SELECT testimonials.id , writer.name AS writer, recipient.name AS recipient ,testimonials.message FROM testimonials JOIN users writer ON testimonials."writerId" = writer.id JOIN users recipient ON testimonials."recipientId" = recipient.id;
 
 -- QUESTÃO 3 ----------------------------
 
-SELECT users.id , users.name , courses.name AS course , schools.name AS school , ed."endDate" AS endDate FROM users JOIN educations ed ON users.id = ed."userId" JOIN courses ON ed."courseId" = courses.id JOIN schools ON ed."schoolId" = schools.id WHERE users.id = 30 LIMIT 5;
+SELECT users.id , users.name , courses.name AS course , schools.name AS school , ed."endDate" AS endDate FROM users JOIN educations ed ON users.id = ed."userId" JOIN courses ON ed."courseId" = courses.id JOIN schools ON ed."schoolId" = schools.id WHERE users.id = 30;
 
 -- QUESTÃO 4 ----------------------------
+
 
