c60778f33ee1a020bac1d8308ec869aa2a17535c
PaulaWiltiner <paulawiltiner.pw@gmail.com>
2022-07-26 20:31:38 -0300

    pratica

--- pratica.sql
+++ pratica.sql
@@ -1,1 +1,11 @@
-SELECT cities.id FROM cities WHERE name="Rio de Janeiro"
+-- QUESTÃO 1 ----------------------------
+
+SELECT users.id , users.name , cities.name AS city  FROM users JOIN cities ON users."cityId" = cities.id WHERE cities.name='Rio de Janeiro' LIMIT 5;
+
+-- QUESTÃO 2 ----------------------------
+
+SELECT testimonials.id , writer.name AS writer, recipient.name AS recipient ,testimonials.message FROM testimonials JOIN users writer ON testimonials."writerId" = writer.id JOIN users recipient ON testimonials."recipientId" = recipient.id LIMIT 5;
+
+-- QUESTÃO 3 ----------------------------
+
+SELECT users.id , users.name , courses.name AS course , schools.name AS school , educations.endDate AS endDate FROM users JOIN educations ON users.id = educations."userId" JOIN courses ON educations."courseId" = courses.id JOIN schools ON educations."schoolId" = schools.id LIMIT 5;
